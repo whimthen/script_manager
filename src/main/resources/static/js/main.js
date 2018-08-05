179f2150f26eb46f20bae2f03222e1aca8ad0cd2
whimthen <whimthen@gmail.com>
2018-08-05 19:55:16 +0800

    完善页面

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,40 @@
+function post(url, data) {
+    $.ajax({
+        //提交数据的类型 POST GET
+        type: "POST",
+        //提交的网址
+        url: url,
+        //提交的数据
+        data: data,
+        //返回数据的格式
+        datatype: "json",//"xml", "html", "script", "json", "jsonp", "text".
+        //在请求之前调用的函数
+        beforeSend: function(){
+            lodding();
+        },
+        //成功返回之后调用的函数
+        success: function(data){
+            $("#msg").html(decodeURI(data));
+            hideLodding();
+        },
+        //调用执行后调用的函数
+        complete: function(XMLHttpRequest, textStatus){
+            alert(XMLHttpRequest.responseText);
+            alert(textStatus);
+            hideLodding();
+        },
+        //调用出错执行的函数
+        error: function(){
+            //请求出错处理
+            hideLodding();
+        }
+    });
+}
+
+function lodding() {
+
+}
+
+function hideLodding() {
+
+}
